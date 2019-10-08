5152119416e2eccc67831eab47fb3e763dad979f
André Botelho <andrebotelho@live.com>
2019-10-07 21:53:59 -0300

    travis mais commit inicial

---
+++ src/Escrita.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Escrita {
+	public static void main (String[] args) {
+		String fileName = "numbers.txt";
+		System.out.println("Quaqluer texto!");
+		try (PrintStream ps = new PrintStream(fileName);) {
+			for (int i = 1; i<=10 ;i++) {
+				ps.printf("%d:%d:%d:%n", i, i*i, i*i*i);
+			}
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
