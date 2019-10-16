06fba8cad68b2de4d4749b0cf4b3652dacf4eee4
laurascclaro <laurascclaro@hotmail.com>
2019-10-16 20:07:47 -0300

    mudar estrutura dos pacotes

---
+++ src/main/java/Leitura.java
@@ -0,0 +1,22 @@
+package src.main;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Leitura {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int somaTotal=0;
+		BufferedReader br = new BufferedReader(new FileReader("numbers.txt"));
+		
+		while(br.ready()){
+			String linha = br.readLine();
+			String[] numbers = linha.split(":");
+			somaTotal += Integer.parseInt(numbers[2]);
+		}
+		br.close();
+		System.out.println("A soma toal da 3 coluna e de: "+somaTotal);
+		
+	}
+}
